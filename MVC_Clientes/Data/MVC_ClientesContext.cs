cd973b4d891566cbb9d18b553099a42f2bd51bd1
Claudia Torres Hernández <claudiatorreshernandez88@gmail.com>
2021-02-23 19:42:11 +0100

    Creando Scaffolding en MVC_Clientes.

---
+++ MVC_Clientes/Data/MVC_ClientesContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MVC_Clientes.Models;
+
+namespace MVC_Clientes.Data
+{
+    public class MVC_ClientesContext : DbContext
+    {
+        public MVC_ClientesContext (DbContextOptions<MVC_ClientesContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MVC_Clientes.Models.Cliente> Cliente { get; set; }
+    }
+}
---
+++ MVC_Clientes/Models/Cliente.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVC_Clientes.Models
+{
+    public class Cliente
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Apellidos { get; set; }
+        public string Direccion { get; set; }
+        public string Telefono { get; set; }
+        
+        [DataType(DataType.Date)]
+        [Display(Name = "Fecha de Alta")]
+        public DateTime FechaAlta { get; set; }
+    }
+}
